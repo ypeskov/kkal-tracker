db83a246432c8fa74fa5db811ee3caf760fea052
Yuriy Peskov <yuriy.peskov@gmail.com>
2025-09-28 11:31:28 +0300

    update build script: docs v1.2.12

--- build-and-push.sh
+++ build-and-push.sh
@@ -6,12 +6,44 @@ TAG="latest"
 PLATFORM_ARG=""
 PUSH=false
 
+# Display help message
+show_help() {
+    cat << EOF
+Usage: $(basename "$0") [OPTIONS] [TAG]
+
+Build and optionally push Docker image for Kkal Tracker application.
+
+OPTIONS:
+    --help                  Show this help message and exit
+    --platform=PLATFORM     Set target platform for the build
+                           (e.g., --platform=linux/amd64)
+    push                    Push the built image to Docker registry
+
+TAG:
+    Optional tag name (default: latest)
+    If provided, the image will be tagged as both TAG and 'latest'
+
+EXAMPLES:
+    $(basename "$0")                    # Build with tag 'latest'
+    $(basename "$0") v1.2.3              # Build with tag 'v1.2.3' and 'latest'
+    $(basename "$0") push                # Build and push with tag 'latest'
+    $(basename "$0") v1.2.3 push         # Build with tag 'v1.2.3' and push
+    $(basename "$0") --platform=linux/arm64 v1.0  # Build for ARM64 platform
+
+The built image will be named: ${IMAGE_NAME}:TAG
+After build, the tag will be written to version.txt file.
+EOF
+}
+
 # Parse arguments
-for arg in "$@"
-do
-    case $arg in
+while [ $# -gt 0 ]; do
+    case $1 in
+        --help|-h)
+        show_help
+        exit 0
+        ;;
         --platform=*)
-        PLATFORM_ARG="$arg"
+        PLATFORM_ARG="$1"
         shift
         ;;
         push)
@@ -19,7 +51,7 @@ do
         shift
         ;;
         *)
-        TAG="$arg"
+        TAG="$1"
         shift
         ;;
     esac
